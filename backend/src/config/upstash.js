fd5d7e7f48f1ad3d5fe65ef751806c67949c1bd1
dheeraj080 <kambledheerajkumar@gmail.com>
2026-01-05 20:45:19 +0530

    new file:   .gitignore 	modified:   backend/node_modules/.package-lock.json 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/src/config/upstash.js 	new file:   backend/src/middleware/rate_limiter.js 	modified:   backend/src/server.js

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import { Ratelimit } from "@upstash/ratelimit"
+import { Redis } from "@upstash/redis"
+
+const redis = Redis.fromEnv()
+
+const ratelimit = new Ratelimit({
+  redis, // pass the instance, not the function
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
+  // optional:
+  // analytics: true,
+  // prefix: "@upstash/ratelimit",
+})
+
+export default ratelimit
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import notes_routes from "./routes/notes_routes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import rate_limiter from "./middleware/rate_limiter.js";
 
 dotenv.config();
 
@@ -13,7 +14,14 @@ const PORT = process.env.PORT || 5001
 
 connectDB()
 
-app.use(express.json()) //middleware
+//middleware
+app.use(express.json()); 
+app.use(rate_limiter);
+
+app.use((req, res, next) => {
+    console.log(`Req method ${req.method} & Req URL ${ReadableByteStreamController.url}`);
+    next();
+})
 
 app.use("/api/notes", notes_routes)
 
