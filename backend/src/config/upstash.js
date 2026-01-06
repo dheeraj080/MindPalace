e9de51a40c65e3985e863cb7f19f5c59678d50b5
dheeraj080 <kambledheerajkumar@gmail.com>
2026-01-06 17:29:21 +0530

    modified:   backend/src/config/upstash.js 	modified:   frontend/src/App.jsx 	modified:   frontend/src/components/Notecard.jsx 	new file:   frontend/src/components/NotesNotFound.jsx 	renamed:    frontend/src/components/RateLimitUI.jsx -> frontend/src/components/RateLimitedUI.jsx 	new file:   frontend/src/lib/axios.js 	new file:   frontend/src/lib/utils.js 	modified:   frontend/src/pages/CreatePage.jsx 	modified:   frontend/src/pages/HomePage.jsx 	modified:   frontend/src/pages/NoteDetailPage.jsx

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -8,7 +8,7 @@ const redis = Redis.fromEnv()
 
 const ratelimit = new Ratelimit({
   redis, // pass the instance, not the function
-  limiter: Ratelimit.slidingWindow(5, "20 s"),
+  limiter: Ratelimit.slidingWindow(22, "20 s"),
   // optional:
   // analytics: true,
   // prefix: "@upstash/ratelimit",
