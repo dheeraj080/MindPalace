3c6ba75ce1d3a13b2e9fc03324d0fc2d3cd1282a
dheeraj080 <kambledheerajkumar@gmail.com>
2026-01-06 18:35:18 +0530

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors";
+import path from "path"
 
 import notes_routes from "./routes/notes_routes.js"
 import { connectDB } from "./config/db.js";
@@ -13,13 +14,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
+
 
 //middleware
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-        origin: "http://localhost:5173",
+      origin: "http://localhost:5173",
     })
-);
+  );
+}
+
 app.use(express.json());
 app.use(rate_limiter);
 
@@ -30,8 +36,18 @@ app.use((req, res, next) => {
 })
 
 
-
 app.use("/api/notes", notes_routes)
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT ", PORT);
